aafc28142a23d918f972c378c807c645e986e599
cemmanuelonyema <cemmanuelonyema@gmail.com>
2024-07-08 20:40:53 +0100

    add hero text and format

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,7 +1,22 @@
+import { Container } from "../../components/container";
+import { Hero, HeroSubtitle, HeroTitle } from "./comps/hero";
+
 function Home() {
   return (
     <>
-      <h1>Home Linear</h1>
+      <Container>
+        <Hero>
+          <HeroTitle>
+            Linear is a better way
+            <br className="hidden md:block" /> to build products
+          </HeroTitle>
+          <HeroSubtitle>
+            Meet the new standard for modern software development.
+            <br className="hidden md:block" /> Streamline issues, sprints, and
+            product roadmaps.
+          </HeroSubtitle>
+        </Hero>
+      </Container>
     </>
   );
 }
